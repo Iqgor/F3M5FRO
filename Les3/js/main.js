30ea33da10c4073e4f00e69036b3ca6110d3ce9d
Igor <herrebrughigor@gmail.com>
2022-10-04 20:00:39 +0200

    les 3 klaar

--- Les3/js/main.js
+++ Les3/js/main.js
@@ -11,6 +11,36 @@ input.oninput = function (event) {
     mono.innerText = event.target.value
 }
 
-const hearts = document.getElementsByClassName("heart")
-
+const hearts = document.getElementsByClassName("heart__logo")
 console.log(hearts)
+
+for(let i = 0; i < hearts.length; i++){
+    hearts[i].onclick = function(event){
+        for(let i = 0; i < hearts.length; i++){
+            hearts[i].classList = "fa-regular fa-heart heart__logo"
+        }
+        hearts[i].classList = "fa-solid fa-heart heart__logo"
+    }
+    
+}
+
+
+const header = document.getElementById("js--header");
+const showcase = document.getElementById("js--showcase")
+const body = document.getElementById("js--body")
+const logo = document.getElementById("js--logo")
+
+const toggle = document.getElementById("js--toggle");
+console.log(toggle)
+toggle.onclick = function(){
+    if (toggle.checked == true){
+        header.style.background = "#06283D"
+        showcase.style.background = "#06283D"
+        showcase.style.color = "#eee"
+        logo.style.background = "#86a79e"
+        input.style.color = "#eee"
+        body.style.background = "#86a79e"
+    }
+    
+}
+
